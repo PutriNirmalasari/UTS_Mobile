40924ac6d01e358caf300fef6b89234ec69012e6
PutriNirmalasari <Putrinirma25@gmail.com>
2023-11-10 16:31:27 +0800

    1

---
+++ app/src/main/java/com/example/loginregisterrelative/MenghitungLuas.kt
@@ -0,0 +1,11 @@
+package com.example.loginregisterrelative
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenghitungLuas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menghitung_luas)
+    }
+}
